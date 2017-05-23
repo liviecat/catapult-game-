db37fab7e615edfc30a775ba0a4e660fe5e4caf2
liviecat <olivia5628@gmail.com>
2017-05-23 17:39:50 +1000

    Second Commit

---
+++ bing/Assets/Scripts/bullet_Behaviour.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class bullet_Behaviour : MonoBehaviour {
+
+
+
+	private Rigidbody my_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		my_Rigidbody = GetComponent<Rigidbody> ();
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//my_Rigidbody.AddForce(Vector3.forward);
+		my_Rigidbody.transform.Translate(Vector3.forward);
+	}
+}
